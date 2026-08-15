1194dd61be3cac516c30d0abacd58a8d56518751
HydroJaL <107747667+HydroJal@users.noreply.github.com>
2026-08-15 20:17:12 +0800

    Delete data directory

--- data/__init__.py
+++
@@ -1,4 +0,0 @@
-from .fetcher import search_company, fetch_financial_data
-from .storage import save_company_data, load_company_data
-
-__all__ = ["search_company", "fetch_financial_data", "save_company_data", "load_company_data"]
